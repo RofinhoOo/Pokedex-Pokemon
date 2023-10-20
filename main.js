03366f16a61cbbe364980acd384a39bf7316d009
Alex Rofes <alexrofes1992@gmail.com>
2023-10-20 14:36:31 +0200

    pokestats

---
+++ main.js
@@ -0,0 +1,6 @@
+const btnFilter = document.querySelector('.icon-filter');
+
+btnFilter.addEventListener('click', () => {
+  const containerFilter = document.querySelector('.container-filters');
+  containerFilter.classList.toggle('active');
+});
